46db04ac4bedbc88b5642319bc463c02d5fe4536
group4BCS1 <cgroup435@gmail.com>
2021-03-24 18:11:01 +0300

    modified readme.md

--- src/Chapter2/exercise5.py
+++ src/Chapter2/exercise5.py
@@ -1,4 +1,6 @@
-celcius_temp = input("Enter temperature: ")
+#Converting degrees Celcius to degrees Fahrenheit
+
+celcius_temp = input("Enter temperature:\n ")
 celcius_temp = int(celcius_temp)
 fahrenheit_temp = (celcius_temp*9/5) + 32
 fahrenheit_temp = int(fahrenheit_temp)
