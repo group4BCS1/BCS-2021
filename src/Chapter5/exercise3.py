fc9485d239764da44353d520177a51c2ef63a6e4
group4BCS1 <cgroup435@gmail.com>
2021-04-22 16:21:07 +0300

    modified src

--- src/Chapter5/exercise3.py
+++ src/Chapter5/exercise3.py
@@ -6,8 +6,13 @@ d = 25 # dimes
 q = 25 # quarters
 o = 0 # one dollar  bill
 f = 0 # five dollar bill
-print( n, 'nickels')
-print( d, 'dimes')
-print( q, 'quarters')
-print( o, 'ones')
-print( f, 'fives')
+print(n, 'nickels')
+print(d, 'dimes')
+print(q, 'quarters')
+print(o, 'ones')
+print(f, 'fives')
+while True:
+    user_input = input('Enter the purchase price:\n ')
+    if user_input == 'q':
+        break
+    print(user_input)                
