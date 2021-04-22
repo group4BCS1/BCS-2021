03393a67eff1da805e21e3e1d7786cd8d4edea35
group4BCS1 <cgroup435@gmail.com>
2021-04-22 16:02:32 +0300

    modified src

---
+++ src/Chapter5/exercise3.py
@@ -0,0 +1,13 @@
+print('Welcome to the vending machine change maker program!')
+print('Change maker initialized')
+print('Stock Contains: ')
+n = 25 # nickels
+d = 25 # dimes
+q = 25 # quarters
+o = 0 # one dollar  bill
+f = 0 # five dollar bill
+print( n, 'nickels')
+print( d, 'dimes')
+print( q, 'quarters')
+print( o, 'ones')
+print( f, 'fives')
