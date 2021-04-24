45d87934d4727584b5a70317df4f53741391caf8
group4BCS1 <cgroup435@gmail.com>
2021-04-24 11:43:59 +0300

    exercise3 second commit

--- src/Chapter5/exercise3.py
+++ src/Chapter5/exercise3.py
@@ -12,6 +12,12 @@ while True:
     try:
         user_input = input("Enter the purchase price (xx.xx) or 'q' to quit:\n ")
         if user_input == 'q': break
+        user_input = float(user_input)
+        user_input = ((user_input * 100)%5==0)
+        if user_input > 0 and ((user_input * 100)%5==0):
+            pass
+        else:
+            print('Invalid input')
         print(user_input)
     except:
         print('Invalid Input')
